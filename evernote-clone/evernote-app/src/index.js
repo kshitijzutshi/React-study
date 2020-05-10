1d6a813d303717114c92297836e5167d389c4253
kshitijzutshi <kshitij.zutshi@gmail.com>
2020-05-10 23:21:39 +0530

    final commit

--- evernote-clone/evernote-app/src/index.js
+++ evernote-clone/evernote-app/src/index.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-const firebase = require('firebase');
-require('firebase/firestore');
-
-
-// AIzaSyAJ9gKwZgAg3K6XZ2junmVdWcDKoc8KgL4
+const firebase = require("firebase");
+require("firebase/firestore");
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,24 +15,16 @@ const firebaseConfig = {
   projectId: "evernote-clone-2d976",
   storageBucket: "evernote-clone-2d976.appspot.com",
   messagingSenderId: "368780706463",
-  appId: "1:368780706463:web:eadb4a65993acf5aa658aa"
+  appId: "1:368780706463:web:eadb4a65993acf5aa658aa",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
-
-
-
-
-
-
-
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('evernote-container')
+  document.getElementById("evernote-container")
 );
 
 // If you want your app to work offline and load faster, you can change
