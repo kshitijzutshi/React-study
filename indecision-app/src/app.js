5594d2a2d70a5cd2e7daaef1e89f3b62481af359
kshitijzutshi <kshitij.zutshi@gmail.com>
2020-05-30 23:07:49 +0530

    imports and exports ES6 webpack

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,1 +1,6 @@
+// import "./utils.js";
+import { square, add } from "./utils.js";
+
 console.log("App is running!!!!!!!!!!!!!!!!");
+console.log(square(4));
+console.log(add(2, 3));
---
+++ indecision-app/src/utils.js
@@ -0,0 +1,5 @@
+const square = (x) => x * x;
+const add = (a, b) => a + b;
+console.log("Utils is runing!!");
+
+export { square, add };
