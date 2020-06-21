9087a3636e8fa7d2723914dbb92361c1c8047983
kshitijzutshi <kshitij.zutshi@gmail.com>
2020-06-21 22:34:26 +0530

    server files changed for socket connection user

--- realtime chat app/server/index.js
+++ realtime chat app/server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 
 const PORT = process.env.PORT || 5000;
 
@@ -13,6 +14,23 @@ const io = socketio(server);
 // Use socket.io to know user connected or diconnected
 io.on("connection", (socket) => {
   console.log("We have a new connection!!");
+
+  socket.on("join", ({ name, room }, callback) => {
+    // console.log(name, room);
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to the room ${user.room}!`,
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name}, has joined!` });
+    socket.join(user.room);
+
+    callback();
+  });
 
   socket.on("disconnect", () => {
     console.log("User has Left!");
